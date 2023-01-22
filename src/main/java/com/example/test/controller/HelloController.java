20c41b556508fd0193747a9821dc529665517cae
mak <mohaddiskhan05@gmail.com>
2023-01-22 19:11:11 +0530

    first commit

--- src/main/java/com/example/test/controller/HelloController.java
+++ src/main/java/com/example/test/controller/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.test.controller;
 
+import com.example.test.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    public TestService testService;
+
     @GetMapping("/greetings")
     public String greet(){
-        return "Hello GitHub";
+        String result=testService.greet();
+        return result;
     }
 }
