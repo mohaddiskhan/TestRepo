b359d0ef15b3efe6e1c4eb777d692962d4dd4e4b
mak <mohaddiskhan05@gmail.com>
2023-01-22 18:55:01 +0530

    first commit

---
+++ src/main/java/com/example/test/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/greetings")
+    public String greet(){
+        return "Hello GitHub";
+    }
+}
