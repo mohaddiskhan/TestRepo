480a3aa49819b56c5606d70cb2a7bacf7f64f171
mak <mohaddiskhan05@gmail.com>
2023-01-23 21:10:52 +0530

    feature changes

--- src/main/java/com/example/test/controller/HelloController.java
+++ src/main/java/com/example/test/controller/HelloController.java
@@ -32,5 +32,10 @@ public class HelloController {
         return "test functionality";
     }
 
+    @GetMapping("/testfeature")
+    public String testFeature(){
+        return "test functionality";
+    }
+
 
 }
